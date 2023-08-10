1593b9235babd717d1890af59552f7980e3994f6
seb lex <ss718@proton.me>
2023-08-10 14:21:34 +0800

    rails task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(posts_params)
+    @task.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(posts_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def posts_params
+    params.require(:task).permit(:title, :details)
+  end
+end
